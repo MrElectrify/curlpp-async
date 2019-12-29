110a3540a6b3f5a75b647c47f55f3d845c562142
Andrew Buck <aman560900@hotmail.com>
2019-12-29 16:50:45 -0500

    Create curlpp-async project, Handle.h, curlpp-asyncTestBench.cpp

---
+++ include/Handle.h
@@ -0,0 +1,19 @@
+#ifndef LIBCURLPP_ASYNC_HANDLE
+#define LIBCURLPP_ASYNC_HANDLE
+
+/*
+ *  CurlPP-Async Handle
+ *  12/29/19 16:11
+ */
+
+namespace CurlPPAsync
+{
+    // Handle allows the spawning of WebClients which asynchronously perform web requests
+    class Handle
+    {
+    public:
+
+    };
+}
+
+#endif
