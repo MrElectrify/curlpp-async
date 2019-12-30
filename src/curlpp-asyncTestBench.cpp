7473c8499e9692987699b1ee068eb4a6b0586133
Andrew Buck <aman560900@hotmail.com>
2019-12-29 23:37:39 -0500

    Added sync test

--- src/curlpp-asyncTestBench.cpp
+++ src/curlpp-asyncTestBench.cpp
@@ -12,7 +12,6 @@ int main()
 	Handle handle;
 	WebClient webClient(handle);
 	WebClient webClient2(handle);
-	WebClient webClient3(handle);
 
 	webClient.SetOpt(CURLOPT_FOLLOWLOCATION, true);
 	webClient.AsyncGET("https://google.com/",
@@ -20,15 +19,28 @@ int main()
 		{
 			if (res != CURLE_OK)
 			{
-				std::cerr << "Res: " << res << '\n';
+				std::cerr << "Async Res: " << res << '\n';
 				return;
 			}
 
-			// GetInfo is not guaranteed to return a valid pointer, but let's try itg
-			std::cout << "Got google, response code: " << webClient.GetInfo<uint32_t>(CURLINFO_RESPONSE_CODE) << ", effective URL: " << webClient.GetInfo<const char*>(CURLINFO_EFFECTIVE_URL) << '\n';
+			// GetInfo is not guaranteed to return a valid pointer, but let's try it
+			std::cout << "Got google asynchronously, response code: " << webClient.GetInfo<uint32_t>(CURLINFO_RESPONSE_CODE) << ", effective URL: " << webClient.GetInfo<const char*>(CURLINFO_EFFECTIVE_URL) << '\n';
 		});
 
+	// Start Async operation
 	handle.Run();
 
+	// now do the same thing, synchronously
+	webClient2.SetOpt(CURLOPT_FOLLOWLOCATION, true);
+	CURLcode res = webClient2.GET("https://google.com/");
+	if (res != CURLE_OK)
+	{
+		std::cerr << "Sync Res: " << res << '\n';
+		return 1;
+	}
+
+	// GetInfo is not guaranteed to return a valid pointer, but let's try it
+	std::cout << "Got google synchronously, response code: " << webClient2.GetInfo<uint32_t>(CURLINFO_RESPONSE_CODE) << ", effective URL: " << webClient2.GetInfo<const char*>(CURLINFO_EFFECTIVE_URL) << '\n';
+	
 	return 0;
 }
