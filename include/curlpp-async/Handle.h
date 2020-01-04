738f4cb5285b5e4411ab640e0b09f2841d0487b4
Andrew Buck <aman560900@hotmail.com>
2020-01-04 15:44:38 -0500

    Added mutexed mid-transfer operation queue overload for Run

--- include/curlpp-async/Handle.h
+++ include/curlpp-async/Handle.h
@@ -14,6 +14,7 @@
 
 // STL
 #include <mutex>
+#include <queue>
 #include <unordered_map>
 
 namespace CURLPPAsync
@@ -35,6 +36,121 @@ namespace CURLPPAsync
 
         // Runs all queued asynchronous operations, and returns when they are all complete
         void Run();
+        // Runs all queued asynchronous operations, adding operations from a mutex-protected 
+        // queue if they appear mid-transfer. Returns when all are complete and the queue is empty
+        // MidTransferOperationQueue example: std::queue<std::function<void()>>
+        // A queue of void returning functions, with front() and pop()
+        // Mutex_t example: std::mutex
+        // A mutex with lock() and unlock()
+        template<typename MidTransferOperationQueue_t, typename Mutex_t>
+        void Run(MidTransferOperationQueue_t& midTransferOperationQueue, Mutex_t& mutex)
+        {
+			int still_running = 0;
+			// try to perform now
+			curl_multi_perform(m_multi, &still_running);
+			while (still_running != 0)
+			{
+				timeval timeout;
+				fd_set fdread;
+				fd_set fdwrite;
+				fd_set fdexcep;
+				int maxfd = -1;
+
+				long curl_timeo;
+
+				curl_multi_timeout(m_multi, &curl_timeo);
+				if (curl_timeo < 0)
+					curl_timeo = 1000;
+
+				timeout.tv_sec = curl_timeo / 1000;
+				timeout.tv_usec = (curl_timeo % 1000) * 1000;
+
+				FD_ZERO(&fdread);
+				FD_ZERO(&fdwrite);
+				FD_ZERO(&fdexcep);
+
+				// get file descriptors from the transfers 
+				CURLMcode mc = curl_multi_fdset(m_multi, &fdread, &fdwrite, &fdexcep, &maxfd);
+				if (mc != CURLM_OK)
+				{
+					// fdset failed, call all handlers with the error
+					for (CallbackMap_t::iterator it = m_callbacks.begin(); it != m_callbacks.end(); ++it)
+					{
+						// tell them to try again
+						const CallbackMap_t::value_type::second_type callback = it->second;
+						it = m_callbacks.erase(it);
+						callback(CURLE_AGAIN);
+					}
+					return;
+				}
+
+				int rc;
+				if (maxfd == -1) {
+#ifdef _WIN32
+					Sleep(100);
+					rc = 0;
+#else
+					/* Portable sleep for platforms other than Windows. */
+					struct timeval wait = { 0, 100 * 1000 }; /* 100ms */
+					rc = select(0, NULL, NULL, NULL, &wait);
+#endif		
+				}
+				else
+					rc = select(maxfd + 1, &fdread, &fdwrite, &fdexcep, &timeout);
+
+				switch (rc) {
+				case -1:
+					// select failed, call all handlers with the error
+					for (CallbackMap_t::iterator it = m_callbacks.begin(); it != m_callbacks.end(); ++it)
+					{
+						// tell them to try again
+						const CallbackMap_t::value_type::second_type callback = it->second;
+						it = m_callbacks.erase(it);
+						callback(CURLE_AGAIN);
+					}
+					break;
+				case 0:
+				default:
+					// timeout or readable/writable sockets
+					curl_multi_perform(m_multi, &still_running);
+					break;
+				}
+
+				// check the transfers and see how they went
+				int msgs_left;
+				CURLMsg* msg;
+				while ((msg = curl_multi_info_read(m_multi, &msgs_left)) != nullptr)
+				{
+					if (msg->msg == CURLMSG_DONE)
+					{
+						// find the callback (which should always be present), call it, and remove it
+						const CallbackMap_t::iterator callbackIt = m_callbacks.find(msg->easy_handle);
+						const CallbackMap_t::value_type::second_type callback = callbackIt->second;
+						m_callbacks.erase(callbackIt);
+
+						// remove the handle
+						curl_multi_remove_handle(m_multi, msg->easy_handle);
+
+						callback(msg->data.result);
+
+						// a callback added a task, keep the loop going
+						if (m_callbacks.size() > 0 &&
+							still_running == 0)
+							++still_running;
+					}
+				}
+
+				{
+					std::lock_guard opGuard(mutex);
+					while (midTransferOperationQueue.empty() == false)
+					{
+						midTransferOperationQueue.front()();
+						midTransferOperationQueue.pop();
+						++still_running;
+					}
+				}
+			}
+        }
     private:
         using CallbackMap_t = std::unordered_map<CURL*, WebClient::RecvCallback_t>;
         
